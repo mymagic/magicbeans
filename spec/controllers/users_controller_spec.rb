7fc5001baff92c09236ee6e03787b34b939599e6
Adrian Ching <achc92@gmail.com>
2014-11-21 12:02:54 +0000

    Updated users_controller_spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe UsersController, :type => :controller do
     
     context 'get new' do
-        it 'assigns @users with All Users in the database' do
+        it 'assigns @user with User in the database' do
             user1 = User.create(name: 'User1', email: 'user1@gmail.com', ic: '111111-11-1111', phone: '010-1111111')
             user2 = User.create(name: 'User2', email: 'user2@gmail.com', ic: '222222-22-2222', phone: '010-2222222')
             
-            get :index
+            get :new
             
-            expect(assigns(:users)).to eq(User.all)
+            expect(assigns(:user)).to eq(User.new)
         end
     end
     
@@ -35,14 +35,5 @@ RSpec.describe UsersController, :type => :controller do
         end
     end
     
-    context 'get index' do
-        it 'assigns @users with All Users in the database' do
-            user1 = User.create(name: 'User1', email: 'user1@gmail.com', ic: '111111-11-1111', phone: '010-1111111')
-            user2 = User.create(name: 'User2', email: 'user2@gmail.com', ic: '222222-22-2222', phone: '010-2222222')
-            
-            get :index
-            
-            expect(assigns(:users)).to eq(User.all)
-        end
-    end
+    
 end
