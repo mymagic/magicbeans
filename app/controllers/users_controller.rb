a4187c0579c249eaadb38dec179301e711dfbb54
Adrian Ching <achc92@gmail.com>
2014-11-21 11:09:53 +0000

    Added Rspec Test For User Model and Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,62 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    
+    def new
+        @user = User.new
+    end
+    
+    def index
+        @users = User.all
+    end
+    
+    def show
+        @user = User.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+        redirect_to(root_url, :notice => 'Record not found')
+    end
+    
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def create
+        @user = User.new(user_params)
+
+        respond_to do |format|
+          if @user.save
+            redirect_to @user, notice: 'User was successfully created.'
+          else
+            render action: 'new' 
+          end
+        end
+    end
+    
+    def update
+        respond_to do |format|
+          if @user.update(user_params)
+            redirect_to @user, notice: 'User was successfully updated.' 
+          else
+            render action: 'edit' 
+          end
+        end
+    end
+    
+    def destroy
+        @user.destroy
+            respond_to do |format|
+              redirect_to users_url 
+            end
+    end
+    
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :age, :email)
+    end
+    
 end
