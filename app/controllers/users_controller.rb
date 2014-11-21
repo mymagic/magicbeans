ec18357fb74917dac074acbc11e4715f2fdf4448
JEVARAAJKUMAR SELVARAJAH <jeva.sanjay@gmail.com>
2014-11-21 21:17:45 +0000

    Used CanCanCan for users controller. Added Admin role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update, :destroy]
-    
+    load_and_authorize_resource
     def new
         @user = User.new
     end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,12 @@ class Ability
     
     user ||= User.new
     
+    if user.is_admin?
+      can :manage, :all
+    else
+      can [:edit, :show], :all
+    end
+    
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
