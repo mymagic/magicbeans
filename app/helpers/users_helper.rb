7e5467a4e9030a3e1d0f5ab478333603169147dd
Imran Ismail <imran.codely@gmail.com>
2014-11-22 10:23:48 +0800

    Remove the now ignored file

--- app/helpers/users_helper.rb
+++
@@ -1,6 +0,0 @@
-module UsersHelper
-	def gravatar_for(email, size)
-        gravatar_id = Digest::MD5::hexdigest(email)
-        gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
-    end
-end
