00b4b162e88a1b6c6fbfac84b1bc87dcea4c847e
AdrianChingHC <achc92@gmail.com>
2014-11-24 22:03:35 +0800

    Added http:// to hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
 
-  config.action_mailer.default_url_options = { host: 'magicbeans.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://magicbeans.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
