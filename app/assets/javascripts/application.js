8f31e91575fdf0f86ad978bf17ffb717f06d758e
Imran Ismail <imran.codely@gmail.com>
2014-11-25 11:14:06 +0800

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,15 +16,3 @@
 //= require bootstrap-sprockets
 //= require_tree .
 //= require dynamic_fields
-
-function remove_fields(link) {
-    $(link).prev("input[type=hidden]").val("1");
-    $(link).closest(".fields").hide();
-}
- 
-function add_fields(link, association, content) {
-    var new_id = new Date().getTime();
-    var regex = new RegExp("new_" + association, "g");
-    $(link).parent().after(content.replace(regex, new_id));
-    $('#new-pilot-fields').modal('show');
-}
