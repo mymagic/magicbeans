193ab9c0e1b8c6c419ce8ba963e8d3c2e902998e
Imran Ismail <imran.codely@gmail.com>
2014-11-25 23:07:11 +0800

    Apply has_role(string role) fix across every controller and models

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,10 +3,4 @@ module UsersHelper
         gravatar_id = Digest::MD5::hexdigest(email)
         gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
     end
-
-    def is_admin?
-    	if user_signed_in?
-    		current_user.roles.include?(Role.find_by_name('Admin'))
-    	end
-    end
 end
