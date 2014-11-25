a17adb7a32f899a90e71f9e02a5b00043386770f
Imran Ismail <imran.codely@gmail.com>
2014-11-26 02:10:25 +0800

    Readded helper is_admin? returns true if current user is admin and false if not

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,12 @@ module UsersHelper
         gravatar_id = Digest::MD5::hexdigest(email)
         gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
     end
+
+    def is_admin?
+        if user_signed_in? && current_user.has_role?('Admin')
+        	true
+        else
+        	false
+        end
+    end
 end
