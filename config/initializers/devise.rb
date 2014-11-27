b74a6117701e542b8ad412ef177ee362fe5595de
AdrianChingHC <achc92@gmail.com>
2014-11-27 18:39:52 +0800

    Configure App ID/KEY and SECRET

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -265,6 +265,7 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
   config.omniauth :facebook, ENV["FACEBOOK_ID"], ENV["FACEBOOK_SECRET"], 
   secure_image_url: 'true',
   image_size: {
@@ -280,5 +281,5 @@ Devise.setup do |config|
         force_login: 'true',
         lang:'en'
       }
-    
+
 end
