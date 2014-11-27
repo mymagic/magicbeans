6f67bc847c85679fe5044e16ad179c292fa4bf32
AdrianChingHC <achc92@gmail.com>
2014-11-27 18:42:05 +0800

    Updated User Model With Omniauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,19 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable,
+         :omniauthable, :omniauth_providers => [:facebook, :twitter]
+
+
+  def self.from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        user.save
+      end
+  end
 
   has_and_belongs_to_many :roles
 
