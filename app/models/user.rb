d298301423fcf644c25025ac170fced5b61887f8
AdrianChingHC <achc92@gmail.com>
2014-11-28 21:32:01 +0800

    Skip Confirmation For Facebook and Twitter Sign Up

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ActiveRecord::Base
         user.uid = auth.uid
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
+        user.skip_confirmation!
         user.save
       end
   end
