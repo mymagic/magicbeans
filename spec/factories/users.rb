3be1e16f739667caee0e55e1a03559b384ac8ba4
Imran Ismail <imran.codely@gmail.com>
2014-11-30 03:16:27 +0800

    Change faker to tally with ic regex validation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,10 +3,9 @@ require 'faker'
 FactoryGirl.define do
   factory :user do |f|
     f.name Faker::Name.name
-    f.ic Faker::Number.number(12)
+    f.ic Faker::Base.numerify('######-##-####')
     f.email Faker::Internet.email
     f.password Faker::Internet.password
     f.phone Faker::PhoneNumber.cell_phone
-    f.confirmed_at Time.now
   end
 end
