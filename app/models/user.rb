6d89f685c16d9fa3d09559f5f301d31865c12fd6
Imran Ismail <imran.codely@gmail.com>
2014-11-29 17:04:10 +0800

    User model presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :name, presence: true
+  validates :phone, presence: true
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
