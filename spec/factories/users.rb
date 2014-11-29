d8860c3405e39e0190694f76ba466856fef021c1
Imran Ismail <imran.codely@gmail.com>
2014-11-30 07:15:40 +0800

    Clean up

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,10 +2,10 @@ require 'faker'
 
 FactoryGirl.define do
   factory :user do |f|
-    f.name Faker::Name.name
-    f.ic Faker::Base.numerify('######-##-####')
     f.email Faker::Internet.email
     f.password Faker::Internet.password
+    f.name Faker::Name.name
+    f.ic Faker::Base.numerify('######-##-####')
     f.phone Faker::PhoneNumber.cell_phone
   end
 end
