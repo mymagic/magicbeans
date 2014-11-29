1aad0c38ab9fb572d04e11bbb8ceee14bbba6a87
Imran Ismail <imran.codely@gmail.com>
2014-11-30 07:14:32 +0800

    Clean ups

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
+  it{is_expected.to respond_to :name, :ic, :phone}
+  let(:subject) { create :user }
+
   it "has a valid factory" do
-    expect(FactoryGirl.create(:user)).to be_valid
+    expect(subject).to be_valid
   end
 
-  it "is invalid without a name" do
-    expect(FactoryGirl.build(:user, name: nil)).not_to be_valid
-  end
+  context 'when updating' do
+    it "should have a name" do
+      expect(build :user, name: nil).not_to be_valid
+    end
 
-  it "is invalid without a ic" do
-    expect(FactoryGirl.build(:user, ic: nil)).not_to be_valid
-  end
+    it "should have an ic" do
+      expect(build :user, ic: nil ).not_to be_valid
+    end
 
-  it "is invalid without a email" do
-    expect(FactoryGirl.build(:user, email: nil)).not_to be_valid
-  end
+    it "should have a unique ic" do
+      expect(build :user, ic: subject.ic).not_to be_valid
+    end
 
-  it "is invalid without a phone" do
-    expect(FactoryGirl.build(:user, phone: nil)).not_to be_valid
-  end
+    it "should have a valid ic" do
+      expect(subject.ic).to match /\A\d{6}-\d{2}-\d{4}\z/
+    end
 
-  it "is invalid without a password" do
-    expect(FactoryGirl.build(:user, password: nil)).not_to be_valid
+    it "should have a phone" do
+      expect(FactoryGirl.build :user, phone: nil).not_to be_valid
+    end
   end
 end
