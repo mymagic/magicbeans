6c7456c946d707bede6af1a83b2397fdd5210312
Imran Ismail <imran.codely@gmail.com>
2014-12-01 23:38:24 +0800

    Rspec user and role HABTM relationship

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Role, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it{is_expected.to respond_to :name}
+
+  let(:role) { create :role }
+  let(:user) { create :user }
+
+  it "has many users" do
+    role.users << user
+    expect(role.users.any?).to be_truthy
+  end
+
+  it "belongs to many users" do
+    user.roles << role
+    expect(user.roles.any?).to be_truthy
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,31 +2,43 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   it{is_expected.to respond_to :name, :ic, :phone}
-  let(:subject) { create :user }
+
+  let(:user) { create :user }
+  let(:role) { create :role }
 
   it "has a valid factory" do
-    expect(subject).to be_valid
+    expect(user).to be_valid
   end
 
   context 'when updating' do
     it "should have a name" do
-      expect(subject.update(name: nil)).to be_falsey
+      expect(user.update(name: nil)).to be_falsey
     end
 
     it "should have a phone" do
-      expect(subject.update(phone: nil)).to be_falsey
+      expect(user.update(phone: nil)).to be_falsey
     end
 
     it "should have an ic" do
-      expect(subject.update(ic: nil)).to be_falsey
+      expect(user.update(ic: nil)).to be_falsey
     end
 
     it "should have a valid ic" do
-      expect(subject.ic).to match /\A\d{6}-\d{2}-\d{4}\z/
+      expect(user.ic).to match /\A\d{6}-\d{2}-\d{4}\z/
     end
 
     it "should have a unique ic" do
-      expect(build :user, ic: subject.ic).not_to be_valid
+      expect(build :user, ic: user.ic).not_to be_valid
     end
+  end
+
+  it "has many roles" do
+    user.roles << role
+    expect(user.roles.any?).to be_truthy
+  end
+
+  it "belongs to many roles" do
+    role.users << user
+    expect(role.users.any?).to be_truthy
   end
 end
