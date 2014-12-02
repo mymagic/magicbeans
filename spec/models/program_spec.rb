6d4fc19dd07c0df669e5cc188e3e7ec403bb1f15
Imran Ismail <imran.codely@gmail.com>
2014-12-02 16:48:26 +0800

    Programs model and factory tests

--- spec/models/program_spec.rb
+++ spec/models/program_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Program, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:subject) { create :program }
+
+  describe 'factory' do
+    it "has a valid factory" do
+      expect(subject).to be_valid
+    end
+  end
+
+  describe 'responses' do
+    it { is_expected.to respond_to :name, :description, :speaker, :speakerbio, :biourl, :keytakeways, :tags, :resources }
+  end
+
+  describe 'relationship' do
+    it { should have_many(:activities) }
+  end
+
+  describe 'accepts attributes' do
+    it { should accept_nested_attributes_for(:activities).allow_destroy(true) }
+  end
 end
