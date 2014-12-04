700fc54f013a6d6767b7d95be5baad3e17727a32
Imran Ismail <imran.codely@gmail.com>
2014-12-05 07:33:56 +0800

    Added activity page, for read, update and delete only.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'sign_in', :sign_out => 'sign_out', :sign_up => 'sign_up'}
   
   resources :programs
-  resources :tweets
+  resources :activities
 
   devise_scope :user do
     root "devise/sessions#new"
