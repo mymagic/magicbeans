c6551a0e3f4c844bbf3fb67ae1e96f87dd402479
Imran Ismail <imran.codely@gmail.com>
2014-12-09 11:25:34 +0800

    After sign in resource not found fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
-  
-  def after_sign_in_path_for(resource_or_scope)
+
+  def after_sign_in_path_for(resource)
     user_path(resource)
   end
   
