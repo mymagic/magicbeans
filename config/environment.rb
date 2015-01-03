a2c06023c2673e29840c710e5704f99aea5930b2
Imran Ismail <imran.codely@gmail.com>
2015-01-03 17:41:44 +0800

    assigned the configs to organizer gem

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Organizer::Config.access_token = Magicbeans.eventbrite_api
+Workspace::Application.config.time_zone = Magicbeans.time_zone
