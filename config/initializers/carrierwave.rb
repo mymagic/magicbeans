1384f4a79247d66968041d118a5d8990ce050a15
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2015-01-04 14:16:42 +0800

    Config fog for production envs.

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,15 @@
+if Rails.env.production? 
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider               => 'AWS',                        # required
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                        # required
+      :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+      :host                   => 's3.example.com',             # optional, defaults to nil
+      :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+    }
+    config.fog_directory  = 'magicbeans'                          # required
+    config.fog_public     = false                                        # optional, defaults to true
+    config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
+  end
+end
