76a702365cdc4691f2c4613226242deb21a8509f
Imran Ismail <imran.codely@gmail.com>
2015-01-13 16:24:55 +0800

    added notification logs

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,29 @@
+function show_logs() {
+    var spinner = $('<img class="center-block img-responsive" width="40" height="40" src="/assets/three-dots.svg">');
+    var notification = $('#notification')
+    notification.empty();
+    spinner.prependTo(notification);
+    $.get( "/logs.json")
+        .done(function(data){
+            $.each(data, function(index, log) {
+                $(  "<li>\
+                  <a href=\"/"+ log.log_type +"/"+ log.type_id +"\">\
+                    <div>\
+                      <i class=\"fa fa-tasks fa-fw\"></i> "+ log.title +"\
+                      <span class=\"text-muted small pull-right\" style=\"margin-top: 2.7px;\">"+ $.timeago(log.created_at) +"</span>\
+                    </div>\
+                  </a>\
+                </li>\
+                <li class=\"divider\"></li>").prependTo(notification)
+            });
+
+            $( "<li>\
+              <a class=\"text-center\" href=\"/logs\">\
+                <strong>See All Audit Log</strong>\
+                <i class=\"fa fa-angle-right\"></i>\
+              </a>\
+            </li>").appendTo(notification);
+            spinner.remove();
+        });
+}
+
