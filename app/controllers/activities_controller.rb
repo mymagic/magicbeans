8c5e7afdc39517d724eb6bdd56b488abe4a61459
AdrianChingHC <achc92@gmail.com>
2015-01-15 00:20:32 +0800

    Add image to tweet

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -102,7 +102,7 @@ class ActivitiesController < ApplicationController
       message = params[:tweet][:message]
       share_event = Organizer.events(id: @activity.event_id).get
       if !message.blank?
-        @send_tweet = client.update(message)
+        @send_tweet = client.update(message + "\n" + share_event.body["url"], File.new(@activity.activity_img.url))
         redirect_to activity_path(@activity), success: 'Successfully tweeted!'
       else      
         redirect_to activity_path(@activity), alert: 'Message cannot be blank. Try again!'
