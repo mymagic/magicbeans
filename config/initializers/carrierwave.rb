77fad6a22e2411dc8ba9fd099c0aaae172823905
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2015-01-15 21:47:27 +0800

    Change to magicbeans-images bucket for S3. Remove optional directives

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,12 +6,12 @@ if Rails.env.production?
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                        # required
-      :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-      :host                   => 's3.example.com',             # optional, defaults to nil
-      :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']#,                        # required
+      # :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+      # :host                   => 's3.example.com',             # optional, defaults to nil
+      # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
     }
-    config.fog_directory  = 'magicbeans'                          # required
+    config.fog_directory  = 'magicbeans-images'                          # required
     config.fog_public     = false                                        # optional, defaults to true
     config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
   end
